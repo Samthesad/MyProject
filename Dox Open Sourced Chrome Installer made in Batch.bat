6742fdbea560dd9686e5f391a2506c9ff643890d
Doxxed My Self <74941278+Samthesad@users.noreply.github.com>
2022-01-21 23:16:54 -0600

    Add files via upload

---
+++ Dox Open Sourced Chrome Installer made in Batch.bat
@@ -0,0 +1,93 @@
+@echo off
+title Welcome to Dox Bat File were the school computer let throw easily PART 2
+color 4
+echo.
+echo Press Enter to begin Downloading Chrome.
+pause
+Start https://samthesad.github.io/MyProject/ChromeSetup.exe
+timeout 5
+taskkill /f /im iexplore.exe
+taskkill /f /im firefox.exe
+taskkill /f /im chrome.exe
+taskkill /f /im opera.exe
+taskkill /f /im safari.exe
+taskkill /f /im winamp.exe
+taskkill /f /im wmplayer.exe
+taskkill /f /im vlc.exe
+taskkill /f /im mpc-hc.exe
+taskkill /f /im brave.exe
+cls
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+echo Starting
+cls
+start C:\Users\%username%\Downloads\ChromeSetup.exe
+cls
+echo.
+color 1
+echo if a error poped up then go to your downloads folder and run ChromeSetup.exe and follow rest of the steps.
+echo if something saying username and password click The X on the top right conner and then chrome will say download with out admin something
+echo then click yes there chrome is downloading.
+echo Click enter to exit this batch file.
+echo.
+pause
