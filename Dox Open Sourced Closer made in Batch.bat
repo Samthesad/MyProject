e3753b762dd14af9f785db41935cd04697d71753
Doxxed My Self <74941278+Samthesad@users.noreply.github.com>
2022-01-21 22:40:31 -0600

    Add files via upload

---
+++ Dox Open Sourced Closer made in Batch.bat
@@ -0,0 +1,24 @@
+@echo off
+title Welcome to Dox Bat File were the school computer let throw easily
+color 4
+echo.
+echo Press Enter to begin Closing all of the tabes that can be closed That I know of.
+pause
+taskkill /f /im Teams.exe
+taskkill /f /im iexplore.exe
+taskkill /f /im firefox.exe
+taskkill /f /im chrome.exe
+taskkill /f /im opera.exe
+taskkill /f /im safari.exe
+taskkill /f /im winamp.exe
+taskkill /f /im wmplayer.exe
+taskkill /f /im vlc.exe
+taskkill /f /im mpc-hc.exe
+taskkill /f /im brave.exe
+taskkill /f /im explorer.exe
+start explorer.exe
+cls
+echo.
+echo Done Press Enter to close
+echo.
+pause
